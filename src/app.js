88ab3c4da487840601efcad7aa780a93b0dad4d2
ori-volfo <ori.volfo@gmail.com>
2017-11-16 09:28:20 +0200

    getting started with redux

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.js';
 import { AppContainer } from 'react-hot-loader';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const store = createStore(reducer, compose(
+   applyMiddleware(thunk),
+   window.devToolsExtension ?  window.devToolsExtension() : f => f
+));
 
 require('./index.html');
 
@@ -12,7 +21,9 @@ const container = document.querySelector('#app-container');
 
 ReactDom.render(
     <AppContainer>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </AppContainer>
     ,container
 );
@@ -23,7 +34,9 @@ if(module.hot){
     module.hot.accept('./components/App', () => {
         ReactDom.render(
             <AppContainer>
-                <App />
+                <Provider store={store}>
+                    <App />
+                </Provider>
             </AppContainer>
             ,container
         )
